ba952d69c076ee72952893f05c9e4d24f56ce8c1
GOVIL K GOWDA <govilkgowda@gmail.com>
2025-07-11 19:11:50 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,13 @@
+print('House is $1M')
+income=int(input("What's your income per annum?  "))
+
+if income>500000:
+     print("Make a Down payment of $300k")
+
+elif income>=300000:
+      print("Make a Down payment of $200k")
+
+else:
+       print("Take a Bank Lone")
+
+        
